ddf7d411107aa543a6b2f28417da17586b13dc7b
Manuel Sousa <mlrcbsousa@gmail.com>
2023-08-05 19:11:26 +0200

    chore: add dev packages and alias in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        quietDeps: true,
+      },
+    },
+  },
   server: {
     port: 3000
   },
