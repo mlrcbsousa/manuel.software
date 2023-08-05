72537a53ffe9739c79d87736455fcc05e594c692
Manuel Sousa <mlrcbsousa@gmail.com>
2023-08-05 19:51:18 +0200

    chore: add pre-commit hooks and lint and yarn scripts

---
+++ src/main.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+
+describe('Simple Vitest test', () => {
+  it('adds two numbers correctly', () => {
+    const result = 2 + 2
+    expect(result).toBe(4)
+  })
+})
