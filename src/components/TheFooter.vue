d02750aa85ece3a23cb53b934cf437255373e101
Manuel Sousa <mlrcbsousa@gmail.com>
2023-08-15 22:24:54 +0100

    Update TheFooter.vue

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        Built by
+        ©️2023 
         <a href="https://www.mlrcbsousa.com" rel="noopener noreferrer" target="_blank">mlrcbsousa.com</a>
         <br />
         <a href="https://github.com/mlrcbsousa/manuel.software">Source Code</a>
