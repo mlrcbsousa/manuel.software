d1d42066f1913cc56f442c5568d32041b3ea9a64
Manuel Sousa <mlrcbsousa@gmail.com>
2023-09-01 13:25:41 +0200

    feat: add copyright icon to footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,12 +1,17 @@
 <template>
   <footer class="footer">
     <div class="content has-text-centered">
-      <p>
-        ©️2023
-        <a href="https://www.mlrcbsousa.com" rel="noopener noreferrer" target="_blank">mlrcbsousa.com</a>
-        <br />
-        <a href="https://github.com/mlrcbsousa/manuel.software">Source Code</a>
-      </p>
+      <span class="icon-text">
+        <span class="icon">
+          <i class="fa-regular fa-copyright"></i>
+        </span>
+        <span
+          >2023
+          <a href="https://www.mlrcbsousa.com" rel="noopener noreferrer" target="_blank">mlrcbsousa.com</a>
+          <br />
+          <a href="https://github.com/mlrcbsousa/manuel.software">Source Code</a>
+        </span>
+      </span>
     </div>
   </footer>
 </template>
