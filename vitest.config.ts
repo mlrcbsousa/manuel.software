b3b9b44286f0d09b1b6703224085802cf74fd3bd
Manuel Sousa <mlrcbsousa@gmail.com>
2023-09-30 15:19:22 +0200

    chore: update packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,19 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
+import { mergeConfig } from 'vitest/config'
 
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      include: ['src/**/*.test.ts'],
-      exclude: ['src/**/*.vue.test.ts'],
-      // https://github.com/bcoe/c8#cli-options--configuration
-      // open coverage/index.html for detailed coverage results
-      coverage: {
-        all: true,
-        reporter: ['cobertura', 'text', 'text-summary', 'html'],
-        include: ['src/**/*.ts'],
-        exclude: ['src/mocks', '**/types', '**/types.ts', '**/config', '**/*.vue', 'src/main.ts', '**/*.test.*'],
-      },
-      environment: 'happy-dom',
+export default mergeConfig(viteConfig, {
+  test: {
+    include: ['src/**/*.test.ts'],
+    exclude: ['src/**/*.vue.test.ts'],
+    // https://github.com/bcoe/c8#cli-options--configuration
+    // open coverage/index.html for detailed coverage results
+    coverage: {
+      all: true,
+      reporter: ['cobertura', 'text', 'text-summary', 'html'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/mocks', '**/types', '**/types.ts', '**/config', '**/*.vue', 'src/main.ts', '**/*.test.*'],
     },
-  }),
-)
+    environment: 'happy-dom',
+  },
+})
